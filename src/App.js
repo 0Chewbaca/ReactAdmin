5b8f86e513fddc964b13aa8ecea9235f9babe6c5
Eren Meric <eren.meric@stu.enka.k12.tr>
2025-07-24 20:42:29 +0200

    Finished FRQ page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 import Calendar from "./scenes/calendar/";
 // import Geography from "./scenes/geography";
 
@@ -36,11 +36,12 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
               {/* 
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
+              
                */}
               {/* <Route path="/geography" element={<Geography />} /> */}
 
